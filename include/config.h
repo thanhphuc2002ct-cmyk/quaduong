2387dd4e18ec14d055e8ffd8aa08b878f6dfaa4b
thanhphuc2002ct-cmyk <thanhphuc2002ct@gmail.com>
2026-06-11 09:38:49 +0700

    11/06/2026

--- include/config.h
+++ include/config.h
@@ -9,10 +9,14 @@
 #define MPU_SDA_PIN 1
 #define MPU_SCL_PIN 2
 // --- Chân điều khiển động cơ ---
-#define M3_INA1 5
-#define M3_INA2 4
-#define M4_INA1 7
-#define M4_INA2 6
+// #define M3_INA1 5
+// #define M3_INA2 4
+// #define M4_INA1 7
+// #define M4_INA2 6
+#define M3_INA1 16
+#define M3_INA2 15
+#define M4_INA1 18
+#define M4_INA2 17
 
 // Tốc độ mặc định
 #define SPEED_DEFAULT 150
